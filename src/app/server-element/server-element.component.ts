508ee05867a3c526a99ddff526f99bd8f38fb2c0
awtw <alec.wang.tpe@gmail.com>
2020-01-23 07:37:05 +0800

    @ContentChild example

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
 	selector: 'app-server-element',
@@ -7,7 +7,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 	@Input('srvElement') element: { type: string; name: string; content: string };
+	@ContentChild('contentParagraph', { static: true })
+	paragraph: ElementRef;
 	constructor() {}
 
 	ngOnInit() {}
+
+	ngAfterContentInit() {
+		console.log(this.paragraph.nativeElement.textContent);
+	}
 }
